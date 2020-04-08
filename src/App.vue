12ee45eec7dc15233717a38ad03dbf3cd11d1ae1
patryk-smakosz <patryksmakosz1@gmail.com>
2020-04-08 06:15:00 +0200

    coordinates sync, dynamic done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
     <div id='app'>
         <Envelope class="envelope"/>
-        <Card class="card"/>
+        <Card class="card" v-if="(windowWidth > 1024)"></Card>
+        <CardPortrait v-else class="card"/>
     </div>
 </template>
 
 <script>
     import Envelope from './assets/envelope.svg?inline';
     import Card from './assets/card.svg?inline';
+    import CardPortrait from './assets/card-portrait.svg?inline';
 
     import gsap from 'gsap';
     import MorphSVGPlugin from 'gsap/MorphSVGPlugin.js';
@@ -20,114 +22,172 @@
 
     export default {
         name: 'app',
-
+        data() {
+            return {
+                windowWidth: window.innerWidth
+            }
+        },
         components: {
             Envelope,
-            Card
+            Card,
+            CardPortrait
         },
         mounted() {
 
 
-            function envelopeAnimation() {
-
-
-                let widthEnvelope = 600;
-
-
+            function envelopeAnimation(envelopeWidth, finalWidth, finalX, finalY, url) {
                 let envelope = document.querySelector('.envelope');
-                let card = document.querySelector('.card');
 
 
-                function percentage(percent, total) {
-                    return ((percent / 100) * total).toFixed(2)
+                function setCardPosition(id) {
+                    let card = document.querySelector('.card');
+
+                    let cardPositionWidth = document.querySelector('#card-position').getBoundingClientRect().width;
+                    let cardPositionPortraitWidth = document.querySelector('#card-position-portrait').getBoundingClientRect().width;
+
+
+                    let fromElement = document.querySelector(".card"),
+                        toElement = document.querySelector(id),
+                        point = {x: 0, y: 0},
+                        convertedPoint = convertForTransforms(
+                            fromElement,
+                            toElement,
+                            point,
+                            {x: 0, y: 0}
+                        );
+
+
+                    function convertForTransforms(fromElement, toElement, fromPoint, toPoint) {
+                        let parent = fromElement.parentNode,
+                            p1 = MotionPathPlugin.convertCoordinates(fromElement, parent, fromPoint),
+                            p2 = MotionPathPlugin.convertCoordinates(toElement, parent, toPoint);
+                        console.log('convert for transforms');
+
+                        return {x: p2.x - p1.x, y: p2.y - p1.y};
+
+                    }
+
+                    if (window.innerWidth < 1024) {
+                        gsap.set(card, {x: convertedPoint.x, y: convertedPoint.y, width: cardPositionPortraitWidth});
+
+                    } else {
+                        gsap.set(card, {x: convertedPoint.x, y: convertedPoint.y, width: cardPositionWidth});
+
+                    }
                 }
 
 
-                const percentOffsetOfCardWidth = percentage(12, widthEnvelope);
-                let widthCard = widthEnvelope - percentOffsetOfCardWidth;
-
-                let fromElement = document.querySelector(".card"),
-                    toElement = document.querySelector("#card-position"),
-                    point = {x: 0, y: 0},
-                    convertedPoint = convertForTransforms(
-                        fromElement,
-                        toElement,
-                        point,
-                        {x: -20, y: 0}
-                    );
-
-
-                // returns the relative distance on the x and y axis.
-                function convertForTransforms(fromElement, toElement, fromPoint, toPoint) {
-                    var parent = fromElement.parentNode,
-                        p1 = MotionPathPlugin.convertCoordinates(fromElement, parent, fromPoint),
-                        p2 = MotionPathPlugin.convertCoordinates(toElement, parent, toPoint);
-                    return {x: p2.x - p1.x, y: p2.y - p1.y};
+                function changexlinkhref(value) {
+                    document.querySelector(".card image").setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', value);
                 }
 
-                gsap.set(envelope, { width: widthEnvelope});
-                gsap.set(card, {x: convertedPoint.x, y: convertedPoint.y, width: widthCard});
-                // console.log(convertedPoint);
-
-                // function changexlinkhref(value) {
-                //     document.querySelector("image").setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', value);
-                // }
-
-                // changexlinkhref(url);
-                GSDevTools.create();
+                changexlinkhref(url);
+                // GSDevTools.create();
                 const tlStart = gsap.timeline();
-                const tlOpen = gsap.timeline();
-                tlOpen
+                const tlOpen = gsap.timeline({paused: true});
 
-                // .to(envelope, {x: 0, y: 0})
-                // .to(card, {x: 0, y: 0})
-                ;
-                // .to('#arrow', {autoAlpha: 0})
-                // // .to('#button', {autoAlpha: 0, scale: 0, transformOrigin: 'center center'}, '<')
-                // .to('#closed', {
-                //     duration: 2,
-                //     transformOrigin: 'center top',
-                //     fill: '#f5f5f5',
-                //     scaleY: -1,
-                //
-                //
-                // })
-                // .from('#pattern-top', {
-                //     duration: 1.3,
-                //     transformOrigin: 'center bottom',
-                //     scaleY: 0,
-                // }, '<0.6')
-                // .from('#paper', {
-                //     duration: 2,
-                //     scaleY: 0,
-                //     transformOrigin: 'center bottom'
-                //
-                // }, '-=1')
-                //
-                //
-                //
-                //
-                // .add('show-paper')
-                // .to(['#pattern-top', '#closed', '#shadows-inner', '#pattern-bottom', '#accents', '#body', '#bottom-shadow'], {
-                //     y: '+=250',
-                //     duration: 2.49
-                //
-                //
-                // }, 'show-paper')
-                // .to('#paper-mask', {y: '250', duration: 2.49}, 'show-paper')
-                //
-                // .to('.envelope', {
-                //     x: 0,
-                //     y: 500,
-                //
-                //     scale: 1
-                //
-                // })
+                if (window.innerWidth > 1024) {
+                    gsap.set(envelope, {width: envelopeWidth, x: 0, y: 0});
+                    setCardPosition("#card-position");
+
+                    tlOpen
+
+                        .to('#arrow', {autoAlpha: 0})
+                        .to('#button', {autoAlpha: 0, scale: 0, transformOrigin: 'center center'}, '<')
+                        .to('#closed', {
+                            duration: 2,
+                            transformOrigin: 'center top',
+                            fill: '#f5f5f5',
+                            scaleY: -1,
 
 
-                // tlStart
-                //     .from('#text > *', {autoAlpha: 0, stagger: 0.1})
-                //     .from('#arrow', {y: '+=10', repeat: 10, yoyo: true, autoAlpha: 0});
+                        })
+                        .from('#pattern-top', {
+                            duration: 1.3,
+                            transformOrigin: 'center bottom',
+                            scaleY: 0,
+                        }, '<0.6')
+                        .from('#paper', {
+                            duration: 2,
+                            scaleY: 0,
+                            transformOrigin: 'center bottom'
+
+                        }, '-=1')
+
+
+                        .add('show-paper')
+                        .to('.envelope', 2.49, {attr: {viewBox: '0 0 700 3000'}}, 'show-paper')
+                        .to(['#pattern-top', '#closed', '#shadows-inner', '#pattern-bottom', '#accents', '#body', '#bottom-shadow'], {
+                            y: '+=250px',
+
+                            duration: 2.49
+
+
+                        }, 'show-paper')
+                        .to('#paper-mask', {y: '250px', duration: 2.49}, 'show-paper')
+
+                        .to('.envelope', {
+
+                            y: '+=500',
+                            autoAlpha: 0
+
+                        })
+                        .to('.card', {width: finalWidth, x: finalX, y: finalY})
+
+
+                } else if (window.innerWidth < 1024) {
+                    gsap.set(envelope, {width: envelopeWidth, x: 0, y: 0});
+                    setCardPosition("#card-position-portrait");
+
+                    tlOpen
+
+                        .to('#arrow', {autoAlpha: 0})
+                        .to('#button', {autoAlpha: 0, scale: 0, transformOrigin: 'center center'}, '<')
+                        .to('#closed', {
+                            duration: 2,
+                            transformOrigin: 'center top',
+                            fill: '#f5f5f5',
+                            scaleY: -1,
+
+
+                        })
+                        .from('#pattern-top', {
+                            duration: 1.3,
+                            transformOrigin: 'center bottom',
+                            scaleY: 0,
+                        }, '<0.6')
+                        .from('#paper-portrait', {
+                            duration: 2,
+                            scaleY: 0,
+                            transformOrigin: 'center bottom'
+
+                        }, '-=1')
+
+
+                        .add('show-paper')
+                        .to('.envelope', 2.49, {attr: {viewBox: '0 0 700 3000'}}, 'show-paper')
+                        .to(['#pattern-top', '#closed', '#shadows-inner', '#pattern-bottom', '#accents', '#body', '#bottom-shadow'], {
+                            y: '+=300px',
+
+                            duration: 2.49
+
+
+                        }, 'show-paper')
+                        .to('#paper-mask-portrait', {y: '300px', duration: 2.49}, 'show-paper')
+
+                        .to('.envelope', {
+
+                            y: '+=500',
+                            autoAlpha: 0
+
+                        })
+                        .to('.card', {width: finalWidth, x: finalX, y: finalY})
+                }
+
+
+                tlStart
+                    .from('#text > *', {autoAlpha: 0, stagger: 0.1})
+                    .from('#arrow', {y: '+=10', repeat: 10, yoyo: true, autoAlpha: 0});
                 const button = document.querySelector('#app');
 
                 button.addEventListener('click', function () {
@@ -136,7 +196,8 @@
                 })
             }
 
-            envelopeAnimation();
+            envelopeAnimation(300, 400, 900, 0, 'https://via.placeholder.com/860x645');
+            // envelopeAnimation(300, 350, 0, 0, 'https://via.placeholder.com/720x1280');
 
 
         }
@@ -145,7 +206,7 @@
 </script>
 
 <style>
-    #closed-shadows, #opened-top, #shadow-paper, #paper-to {
+    #closed-shadows, #opened-top, #shadow-paper, #paper-to, #card-position, #card-position-portrait {
         visibility: hidden;
     }
 
@@ -189,33 +250,13 @@
         pointer-events: none;
     }
 
-    /*.card {*/
-    /*    position: absolute;*/
-    /*    left: 50%;*/
-    /*    top: 50.5%;*/
-    /*    width: 539px;*/
-    /*    height: auto;*/
-    /*    transform: translate(-50%, -50%);*/
-    /*}*/
-
-    /*.envelope {*/
-    /*    position: absolute;*/
-    /*    left: 50%;*/
-    /*    top: 50%;*/
-    /*    width: 600px;*/
-    /*    height: auto;*/
-    /*    transform: translate(-50%, -50%);*/
-
-    /*}*/
-
 
     .card {
         position: absolute;
         left: 0;
         top: 0;
-        width: 539px;
         height: auto;
-        /*transform: translate(-50%, -50%);*/
+        transform: translate(0, 0);
 
     }
 
@@ -223,7 +264,7 @@
         position: absolute;
         left: 0;
         top: 0;
-        width: 600px;
+        width: 300px;
         height: auto;
 
     }
